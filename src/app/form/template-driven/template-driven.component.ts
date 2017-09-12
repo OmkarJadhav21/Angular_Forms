8841d07595d9445704996aa2f7de8d77e131910d
MacMini_Techmodi <you@example.com>
2017-09-12 18:43:04 +0530

    2nd way to read data of form

--- src/app/form/template-driven/template-driven.component.ts
+++ src/app/form/template-driven/template-driven.component.ts
@@ -7,13 +7,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./template-driven.component.css']
 })
 export class TemplateDrivenComponent implements OnInit {
-
+frmData={
+  usNm:'',
+  eml:'',
+  pass:''
+}
   constructor() { }
 
   ngOnInit() {
   }
-  onSubmit(myFrm:NgForm){
-console.log(myFrm.form.value);
-
+  onSubmit(myFrm?:NgForm){
+// console.log(myFrm.form.value);       //first way to read data of form at console
+console.log(this.frmData);           //Second way to read data of form at console
   }
 }
