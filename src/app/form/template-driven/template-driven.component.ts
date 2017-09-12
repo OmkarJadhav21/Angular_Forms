24b38aaae1ab7497efccd5bc8da2da47a349458d
MacMini_Techmodi <you@example.com>
2017-09-12 18:33:16 +0530

    Reading Forms (1) By LocalReference-@console

--- src/app/form/template-driven/template-driven.component.ts
+++ src/app/form/template-driven/template-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
@@ -11,5 +12,8 @@ export class TemplateDrivenComponent implements OnInit {
 
   ngOnInit() {
   }
+  onSubmit(myFrm:NgForm){
+console.log(myFrm.form.value);
 
+  }
 }
