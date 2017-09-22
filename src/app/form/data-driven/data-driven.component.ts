0483383c4a447b3570ab8cbe603fc29e3f22bffe
MacMini_Techmodi <you@example.com>
2017-09-22 13:35:45 +0530

    Data driven Validation

--- src/app/form/data-driven/data-driven.component.ts
+++ src/app/form/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
@@ -13,12 +13,24 @@ export class DataDrivenComponent implements OnInit {
 
   ngOnInit() {
   this.myform=this.fb.group({
-    user:[''],
-    email:[''],
-    pass:['']
+    user:['', Validators.required],
+    email:['', Validators.compose([
+    
+      Validators.required,       
+      Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+     //this is emails regular expression pattern
+  ])],
+    pass:['', Validators.compose([
+      Validators.required,
+     this.length10
+    ])]
 })
   }
   dataSubmit(){
     console.log(this.myform);    
   }
+  length10(control: AbstractControl): ValidationErrors | null {
+    return control.value.length >= 8   ? null : { myErr: 'Check the length' };
+  }
+
 }
